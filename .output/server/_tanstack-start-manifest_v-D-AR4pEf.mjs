90220e5af15412e46eb624f6be6e7fe795a1ebee
AIME-KPODAR <seuildigital@gmail.com>
2026-07-04 15:21:22 +0000

    Remove Bun configuration

---
+++ .output/server/_tanstack-start-manifest_v-D-AR4pEf.mjs
@@ -0,0 +1,25 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-D-AR4pEf.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "E:/MiabeClass/src/routes/__root.tsx",
+		children: ["/", "/enrollment"],
+		preloads: ["/assets/index-D9LCqbIt.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-D9LCqbIt.js"
+		} }]
+	},
+	"/": {
+		filePath: "E:/MiabeClass/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-DnW1EkBe.js"]
+	},
+	"/enrollment": {
+		filePath: "E:/MiabeClass/src/routes/enrollment.tsx",
+		children: void 0,
+		preloads: ["/assets/enrollment-D4tUvcU8.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
